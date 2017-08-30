f650f1f5dc1cdf0ca9438e50ea428754c7645bf4
anil <anil.bluebash@gmail.com>
2017-08-30 23:04:02 +0530

    change in books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books = current_user.books
   end 
 
   def show
@@ -12,12 +12,13 @@ class BooksController < ApplicationController
 	end
  
   def edit
-    @book = Book.find(params[:id])
+  @book = Book.find(params[:id])
   end
 
   def create
 		@book = Book.new(book_params)
-    
+
+    @book.user_id = current_user.id
 	  if @book.save
 		  redirect_to @book
 	  else
@@ -27,7 +28,6 @@ class BooksController < ApplicationController
 
 	def update
     @book = Book.find(params[:id])
- 
     if @book.update(book_params)
       redirect_to @book
     else
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
     @book.destroy
  
     redirect_to books_path
-  end
+end
 
 	private
   def book_params
